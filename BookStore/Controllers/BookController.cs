bd6f08211a666618bf11f0a4ce02a0c7d7a24c99
HarmeshGIT <headboy6@gmail.com>
2020-12-08 14:11:08 +0530

    Created new BookStore app to the server

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController :Controller
+    {
+
+        public string Index()
+        {
+            return "Hello Harmesh";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"you are getting book ={id}";
+        }
+    }
+}
